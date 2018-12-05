033912ba25c8408c08d441b681eb585685a6c26e
Aaron Klump <sourcecode@intheloftstudios.com>
2018-12-04 17:15:34 -0800

    Build docs.

--- .web_package/hooks/build/00_compile_docs.php
+++ .web_package/hooks/build/00_compile_docs.php
@@ -1,14 +1,21 @@
 <?php
 
+/**
+ * @file
+ * Generates documentation, adjusts paths and adds to SCM.
+ */
+
 namespace AKlump\WebPackage;
 
 $build
   ->generateDocumentation()
+  // This will adjust the path to the image, pulling it from docs.
   ->loadFile('README.md')
   ->replaceTokens([
     'images/smart-images.jpg' => 'docs/images/smart-images.jpg',
   ])
   ->saveReplacingSourceFile()
+  // Add some additional files to SCM that were generated and outside of the docs directory.
   ->addFilesToScm([
     'README.md',
     'CHANGELOG.md',
