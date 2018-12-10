cbc781827553dc83348a0c70b36d754bbd1b95a5
Aaron Klump <sourcecode@intheloftstudios.com>
2018-12-10 10:26:02 -0800

    Rewrite demo for new BreakpointX version.

--- .web_package/hooks/build/60_demo.sh
+++ .web_package/hooks/build/60_demo.sh
@@ -3,6 +3,6 @@
 # @file
 # Copy relevent files from parent into demo
 #
-
+[[ -e demo/smart-images.js ]] && rm demo/smart-images.js
 cp dist/smart-images.js demo/
 cd demo && yarn
--- .web_package/hooks/dev/9_demo.sh
+++ .web_package/hooks/dev/9_demo.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # Point demo dist to main dist
-cd "$7/demo/" && rm smart-images.js && ln -s "$7/smart-images.js"
+cd "demo/" && rm smart-images.js && ln -s "../src/smart-images.js"
